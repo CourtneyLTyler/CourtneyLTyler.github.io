a0ab18deb4e0edffd1a98e435eda937eb34ca933
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-20 02:38:02 -0400

    add thumbnails for projects

--- js/scripts.js
+++ js/scripts.js
@@ -19,36 +19,33 @@ $(document).ready(function(){
     var works = [
         {
             title: "Towers of Hanoi",
-            // pic: "img/tower.png",
-            pic: "img/work3.jpeg",
+            pic: "img/towers250.png",
 			link: "https://courtneyltyler.github.io/GA-three-towers/",
 			info: "Towers of Hanoi: Strategy game created with HTML, CSS, and JavaScript"
         },
         {
             title: "Concert Diary",
-            // pic: "img/cdOrig200.png",
-            pic: "img/work4.jpeg",
+            pic: "img/cd250.png",
 			link: "https://concertdiary.herokuapp.com/",
 			info: "Concert Diary: Full-Stack Web Application utilizing Mongoose, Express, Handlebars, Node"
         },
         {
             title: "DevBook",
-            // pic: "img/devbook200.png",
-            pic: "img/work2.jpeg",
+            pic: "img/devbook250.png",
 			link: "http://dev-book.surge.sh/",
 			info: "DevBook: Full-Stack Social Media Application utilizing Mongoose, Express, React, Node"
         },
         {
-            title: "Scheduler",
+            title: "Coming Soon!",
             pic: "img/work6.jpeg",
 			link: "#",
-			info: "Scheduler: Full-Stack Scheduling Web Application utilizing Python, Django, Postgresql, React"
+			info: "Scheduler: Full-Stack Scheduling Application utilizing Python, Django, Postgresql, React"
         }
     ];
 
     for(var i = 0; i < works.length; ++i ) {
         $("#work-row").append("\
-        <div class='col-lg-3 col-md-3 col-sm-6 col-xs-6'>\
+        <div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>\
             <a href='" + works[i].link +"' class='work-img'>\
             <img src='" + works[i].pic +"' class='img-responsive'>\
             <span class='info'><p class='proj-title'>Title:</p>'" + works[i].title +"' </span>\
