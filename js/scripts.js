409857a8472719bae383550a9d9b1cbe7bed0a71
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-20 01:38:49 -0400

    clean up site and code

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,19 @@
 // Google maps
 var map;
 function initMap() {
-  var myLatLng = {lat: 38.8935124, lng: -77.0499472};
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 10
-  });
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map
-  });
+	var myLatLng = {lat: 38.8935124, lng: -77.0499472};
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		zoom: 10
+	});
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map
+	});
 }
 
 // load document first
 $(document).ready(function(){
-    
-    // tooltips
-     $(function () {
-       $('#tooltip').tooltip();
-     });
-  
-  // what happens after submit button is clicked
-  $("#submit").on("click", function() {
-  
-    if ($('#message-box').val() == ""){
-      $('#message-box').css('border', '3px red solid');
-    } else {
-      $('#message-box').css('border', 'none');
-        var comment = $('#message-box').val();
-        console.log(comment);
-      $('#visible-comment').html(comment);
-      $('#message-box').hide();
-      $('#email').hide();
-      $('#phone').hide();
-      $('#name').hide();
-      document.getElementsByTagName('form').reset();
-      $('name').val() == ""; 
-      alert('Information Submitted');
-      return false;
-    }
-  });
 
 // work section
     var works = [
@@ -47,24 +21,28 @@ $(document).ready(function(){
             title: "Towers of Hanoi",
             // pic: "img/tower.png",
             pic: "img/work3.jpeg",
-            link: "https://courtneyltyler.github.io/GA-three-towers/"
+			link: "https://courtneyltyler.github.io/GA-three-towers/",
+			info: "Towers of Hanoi: Strategy game created with HTML, CSS, and JavaScript"
         },
         {
             title: "Concert Diary",
             // pic: "img/cdOrig200.png",
             pic: "img/work4.jpeg",
-            link: "https://concertdiary.herokuapp.com/"
+			link: "https://concertdiary.herokuapp.com/",
+			info: "Concert Diary: Full-Stack Web Application utilizing Mongoose, Express, Handlebars, Node"
         },
         {
             title: "DevBook",
             // pic: "img/devbook200.png",
             pic: "img/work2.jpeg",
-            link: "http://dev-book.surge.sh/"
+			link: "http://dev-book.surge.sh/",
+			info: "DevBook: Full-Stack Social Media Application utilizing Mongoose, Express, React, Node"
         },
         {
             title: "Scheduler",
             pic: "img/work6.jpeg",
-            link: "#"
+			link: "#",
+			info: "Scheduler: Full-Stack Scheduling Web Application utilizing Python, Django, Postgresql, React"
         }
     ];
 
@@ -74,7 +52,8 @@ $(document).ready(function(){
             <a href='" + works[i].link +"' class='work-img'>\
             <img src='" + works[i].pic +"' class='img-responsive'>\
             <span class='info'><p class='proj-title'>Title:</p>'" + works[i].title +"' </span>\
-            </a>\
+			</a>\
+			<h4 class='project-info'>" + works[i].info +"</h3>\
         </div>\
         ");
     $(".work-img").mouseenter( function() {
